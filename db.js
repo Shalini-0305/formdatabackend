080cfdb7ccdf17e1097ee68af909ac8fa7149192
shalini-0305 <shalinishalini11830@gmail.com>
2026-06-26 11:27:56 +0530

    changes

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const connectdb = () =>{
     try{
-        mongoose.connect("mongodb://localhost:27017");
+        mongoose.connect("mongodb+srv://shalinicseng_db_user:Ih0aNLoVU4kIDE2Q@cluster0.2cls5d2.mongodb.net/");
         console.log("DB Connected");
     }
     catch(err){
