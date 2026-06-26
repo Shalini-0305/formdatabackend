58a5b54a46cdc8778069f669fd4d841a815098de
shalini-0305 <shalinishalini11830@gmail.com>
2026-06-26 12:41:23 +0530

    env fixed and db working

--- db.js
+++ db.js
@@ -1,10 +1,21 @@
+// import mongoose from "mongoose";
+// export const connectdb = () =>{
+//     try{
+//         mongoose.connect("mongodb+srv://shalinicseng_db_user:Ih0aNLoVU4kIDE2Q@cluster0.2cls5d2.mongodb.net/");
+//         console.log("DB Connected");
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// }
+
 import mongoose from "mongoose";
-export const connectdb = () =>{
-    try{
-        mongoose.connect("mongodb+srv://shalinicseng_db_user:Ih0aNLoVU4kIDE2Q@cluster0.2cls5d2.mongodb.net/");
-        console.log("DB Connected");
-    }
-    catch(err){
-        console.log(err);
-    }
-}
+
+export const connectdb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("DB Connected");
+  } catch (err) {
+    console.log("DB Error:", err.message);
+  }
+};
--- server.js
+++ server.js
@@ -1,18 +1,46 @@
-import app from "./app.js";
-import { connectdb } from "./db.js";
+// import app from "./app.js";
+// import { connectdb } from "./db.js";
 
-connectdb()
+// connectdb()
 
-app.get("/data",(req,res)=>{
-    res.send("api is running");
-})
-
-// app.listen(5000,(req,res)=>{
-//     console.log("Backend running");
+// app.get("/data",(req,res)=>{
+//     res.send("api is running");
 // })
 
-const PORT = process.env.PORT || 5000;
+// // app.listen(5000,(req,res)=>{
+// //     console.log("Backend running");
+// // })
 
-app.listen(PORT, () => {
-  console.log(`Backend running on port ${PORT}`);
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Backend running on port ${PORT}`);
+// });
+
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectdb } from "./db.js";
+
+// Load .env FIRST
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+// connect DB and then start server
+connectdb().then(() => {
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log("Backend running on port", PORT);
+  });
 });
