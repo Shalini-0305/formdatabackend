d1a695836c3eddbaee19a7d3741bc75ec31bf006
shalini-0305 <shalinishalini11830@gmail.com>
2026-06-26 11:33:12 +0530

    data

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ app.get("/data",(req,res)=>{
     res.send("api is running");
 })
 
-app.listen(5000,(req,res)=>{
-    console.log("Backend running");
-})
+// app.listen(5000,(req,res)=>{
+//     console.log("Backend running");
+// })
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+});
